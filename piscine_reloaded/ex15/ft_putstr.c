c322434fb8d97b8c6732caa20e7647f5b0bdb9e4
Jessica Sales Pereira Dos Santos <jsales-p@c3r11p5.42sp.org.br>
2025-07-14 18:01:43 -0300

    primeira tentativa enviada e corrigida parcialmente

--- piscine_reloaded/ex15/ft_putstr.c
+++ piscine_reloaded/ex15/ft_putstr.c
@@ -6,14 +6,11 @@
 /*   By: jsales-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:08:01 by jsales-p          #+#    #+#             */
-/*   Updated: 2025/07/11 18:12:46 by jsales-p         ###   ########.fr       */
+/*   Updated: 2025/07/14 17:11:03 by jsales-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
