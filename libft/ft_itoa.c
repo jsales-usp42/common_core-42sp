9d84f678a99bd982a75c7f0b3a34cc9c7408fc0a
jsales-p <jessicasales@DESKTOP-HSAJRN0.localdomain>
2025-08-20 22:39:54 -0300

    fixing ft_itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -10,4 +10,74 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// Function that ...
+#include "./libft.h"
+
+int	size_string(int n)
+{
+	int	size;
+
+	if (n == 0)
+		return (2);
+	if (n == -2147483648)
+		return (12);
+	size = 0;
+	if (n < 0)
+	{
+		size++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		size++;
+	}
+	return (size + 1);
+}
+
+char	*if_zero(char *str)
+{
+	str[0] = '0';
+	str[1] = '\0';
+	return (str);
+}
+
+int	if_negative(int n, int *is_negative)
+{
+	unsigned int	num;
+
+	*is_negative = -1;
+	if (n == -2147483648)
+		num = 2147483648;
+	else
+		num = (unsigned int)(-n);
+	return (num);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	unsigned int	num;
+	int	i;
+	int	is_negative;
+
+	i = size_string(n) - 1;
+	is_negative = 0;
+	str = malloc(size_string(n) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+		return (if_zero(str));
+	if (n < 0)
+		num = if_negative(n, &is_negative);
+	else
+		num = (unsigned int)n;
+	while (num > 0)
+	{
+		str[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	if (is_negative)
+		str[i] = '-';
+	return (str);
+}
